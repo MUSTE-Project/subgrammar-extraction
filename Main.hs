b41d94c2e7fa505d0a47e9099bcb5b4106ff96eb
Herbert Lange <herbert.lange@cse.gu.se>
2020-01-21 14:56:56 +0100

    update main function to run the subtree test

--- Main.hs
+++ Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Subgrammar.GFSubtree
+import Subgrammar.Common
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = -- putStrLn "Hello, Haskell!"
+  do
+    _ <- time Subgrammar.GFSubtree.test
+    return ()
