5c4e443855574a467f010841c7cbd8b007a46782
Herbert Lange <herbert.lange@cse.gu.se>
2020-02-12 12:00:52 +0100

    fix issues with writing file, some minor fixes and added time to output

--- Subgrammar/Experiments.hs
+++ Subgrammar/Experiments.hs
@@ -13,14 +13,16 @@ import Subgrammar.Common
 import Subgrammar.GFSubtree
 import Control.Monad
 import Data.LinearProgram.GLPK.IO
+import System.IO
 
 -- Enables debugging
 debug = True
 
 -- | Returns the rules and the associated precision and recall
-recreateFromExamples :: Grammar -> Language -> Grammar -> [Example] -> Int -> ObjectiveFunction [(String, [String])] -> IO ([String],Double,Double)
+recreateFromExamples :: Grammar -> Language -> Grammar -> [Example] -> Int -> ObjectiveFunction [(String, [String])] -> IO (Integer,[String],Double,Double)
 recreateFromExamples g_r lang_r g_0 examples maxSubtreeSize ofun =
   do
+    start <- startTimer
     -- putStrLn $ ">>> Working on " ++ show examples
     let forests = examplesToForests g_r lang_r examples
     -- create csp
@@ -35,26 +37,29 @@ recreateFromExamples g_r lang_r g_0 examples maxSubtreeSize ofun =
     let splitted = filter (/= hole) $ concat [split "#" r|r <- snd solution]
     let precision = (fromIntegral $ length (intersect (map showCId $ functions $ pgf g_0) splitted)) / (fromIntegral $ length (intersect (map showCId $ functions $ pgf g_r) splitted))
     let recall = (fromIntegral $ length (intersect (map showCId $ functions $ pgf g_0) splitted)) / (fromIntegral $ length (functions $ pgf g_0))
-    return (splitted, precision,recall)
+    time <- stopTimer start
+    return (time,splitted, precision,recall)
 
 -- | Return the examples used, the rules created, precision and recall
-recreateGrammar :: Grammar -> Language -> Grammar -> Int -> Int -> Int -> Int -> ObjectiveFunction [(String, [String])] -> IO [([String],[String],Double,Double)]
-recreateGrammar g_r lang_r g_0 exampleCount treeDepth maxSubtreeSize repetitions ofun = do
+recreateGrammar :: Grammar -> Language -> Grammar -> Int -> Int -> Int -> Int -> ObjectiveFunction [(String, [String])] -> IO [(Int,Int,Integer,[String],[String],Double,Double)]
+recreateGrammar g_r lang_r g_0 maxExampleCount treeDepth maxSubtreeSize repetitions ofun = do
   let gen = mkStdGen 4 -- chosen by a fair dice role
   when debug $ putStrLn "  >>> Generate trees"
-  let trees = take exampleCount $ generateRandomDepth gen (pgf g_0) (startCat $ pgf g_0) (Just treeDepth)
+  let trees = take maxExampleCount $ generateRandomDepth gen (pgf g_0) (startCat $ pgf g_0) (Just treeDepth)
   when debug $ putStrLn "  >>> Linearize trees"
   let sentences = [linearize (pgf g_r) lang_r t | t <- trees]
-
   when debug $ putStrLn "  >>> Randomize sentences"
---  let shuffledSentences = if repetitions > 1 then map (\l -> shuffle' l (length l) gen) $ replicate repetitions sentences else [sentences]
   shuffledSentences <- sequence (replicate (fromIntegral repetitions) (generate (shuffle sentences)))
   when debug (putStrLn $ show shuffledSentences)
   when debug $ putStrLn "  >>> Start process"
-  sequence [(\(r,prec,re) -> (es,r,prec,re)) <$> recreateFromExamples g_r lang_r g_0 es maxSubtreeSize ofun | shuffled <- shuffledSentences,
-             l <- [1..length shuffled], let es = (take l shuffled)]
-    
---  withPool 4 $ \p -> parallel p
+  sequence [(\(time,rules,prec,recall) -> (count,exampleCount,time,examples,rules,prec,recall)) <$> recreateFromExamples g_r lang_r g_0 examples maxSubtreeSize ofun
+           | (count,shuffled) <- zip [1..] shuffledSentences,
+             exampleCount <- [1..length shuffled],
+             let examples = (take exampleCount shuffled)
+           ]
+
+reshufflingCount = 3
+maxExampleCount = 5
 
 recreateExemplum :: FilePath -> IO ()
 recreateExemplum outFile = 
@@ -70,18 +75,28 @@ recreateExemplum outFile =
     pgf_0_fin <- readPGF $ "pgfs/ExemplumFin.pgf"
     pgf_0_swe <- readPGF $ "pgfs/ExemplumSwe.pgf"
     when debug $ putStrLn ">>> Work Work Work"
-    writeFile outFile =<< ("\"ExampleCount\";\"TreeDepth\",\"SubtreeSize\";\"ObjectiveFunction\";\"Precission\";\"Recall\";\"Rules\";\"Examples\"\n" ++) <$> unlines <$> sequence
-      [(recreateGrammar (Grammar lpgf_r []) (fromJust $ readLanguage lname) (Grammar lpgf_0 []) exampleCount treeDepth maxSubtreeSize repetitions ofun >>=
-         (\results -> return $ concat
-                      [(show exampleCount ++ ";" ++ show treeDepth ++ ";" ++ show maxSubtreeSize ++ ";" ++ show repetitions ++ ";" ++ show oname ++ ";" ++
-                        show prec ++ ";" ++ show recall ++ ";" ++ (show $ show rules) ++ ";" ++ (show $ show examples)) | (examples,rules,prec,recall) <- results]
-         )
-       ) :: IO String
-      | exampleCount <- [1..20], treeDepth <- [4..6], maxSubtreeSize <- [1..3], repetitions <- [1..5],
-        (oname,ofun) <- [("numTrees",numTrees),("numRules",numRules)], (lname,lpgf_r,lpgf_0) <- [("LangEng",pgf_r_eng,pgf_0_eng),("LangGer",pgf_r_ger,pgf_0_ger),("LangFin",pgf_r_fin,pgf_0_fin),("LangSwe",pgf_r_swe,pgf_0_swe)]]
-      -- | exampleCount <- [1..10], treeDepth <- [4..5], maxSubtreeSize <- [1..2], repetitions <- [1..2],
-      --   (oname,ofun) <- [("numRules",numRules)],(lname,lpgf_r,lpgf_0) <- [("LangEng",pgf_r_eng,pgf_0_eng),("LangGer",pgf_r_ger,pgf_0_ger),("LangFin",pgf_r_fin,pgf_0_fin),("LangSwe",pgf_r_swe,pgf_0_swe)]]
-                                                                                                                                                                                    )
+    withFile outFile WriteMode
+      (\handle ->
+          do
+            hSetBuffering handle NoBuffering
+            hPutStrLn handle "\"ShuffleNo\";\"ExampleCount\";\"TreeDepth\";\"SubtreeSize\";\"ObjectiveFunction\";\"Time\";\"Precision\";\"Recall\";\"Rules\";\"Examples\""
+            sequence 
+              [(recreateGrammar (Grammar lpgf_r []) (fromJust $ readLanguage lname) (Grammar lpgf_0 []) maxExampleCount treeDepth maxSubtreeSize reshufflingCount ofun >>=
+                 (\results ->
+                    hPutStrLn handle $ unlines
+                      [(show shuffleNo ++ ";" ++ show exampleCount ++ ";" ++ show treeDepth ++ ";" ++ show maxSubtreeSize ++ ";" ++ show oname ++ ";" ++ show time ++ ";" ++
+                        show prec ++ ";" ++ show recall ++ ";" ++ (show $ show rules) ++ ";" ++ (show $ show examples))
+                      | (shuffleNo,exampleCount,time,examples,rules,prec,recall) <- results]
+                 )
+               )
+      -- | exampleCount <- [1..20], treeDepth <- [4..6], maxSubtreeSize <- [1..3], repetitions <- [1..5],
+      --   (oname,ofun) <- [("numTrees",numTrees),("numRules",numRules)], (lname,lpgf_r,lpgf_0) <- [("LangEng",pgf_r_eng,pgf_0_eng),("LangGer",pgf_r_ger,pgf_0_ger),("LangFin",pgf_r_fin,pgf_0_fin),("LangSwe",pgf_r_swe,pgf_0_swe)]]
+              | treeDepth <- [4..5], maxSubtreeSize <- [1..2],
+                (oname,ofun) <- [("numRules",numTrees)],
+                (lname,lpgf_r,lpgf_0) <- [("LangEng",pgf_r_eng,pgf_0_eng)]]
+      )
+    return ()
+      
 compareTreebank :: Grammar -> Language -> [(String,Tree)] -> Int -> ObjectiveFunction [(String,[String])] -> IO (Double,Double)
 compareTreebank g_r lang_r treeBank maxSubtreeSize ofun =
   do
