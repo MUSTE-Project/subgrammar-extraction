deca5f404fdbb30e82f808a46d8b2886915e171e
Herbert Lange <herbert.lange@cse.gu.se>
2020-02-19 17:13:04 +0100

    work on negative examples

--- Subgrammar/GFRule.hs
+++ Subgrammar/GFRule.hs
@@ -5,40 +5,53 @@ import Data.Maybe
 import Control.Monad.LPMonad
 import Data.LinearProgram
 import System.FilePath((</>))
+import Data.List
 
 import PGF
   
 -- Translate a list of forests into a constraint problem
-forestsToProblem :: [Forest] -> ObjectiveFunction [String] -> Problem
-forestsToProblem forests (OF f dir) = 
+forestsToProblem :: [Forest] -> [Forest] -> ObjectiveFunction [String] -> Problem
+forestsToProblem positive_forests negative_forests (OF f dir) = 
   let
     -- helper to add consequtive numbers
     numbered :: [a] -> [(Int,a)]
     numbered = zip [1..]
     -- Hierarchy of tags for sentences, trees and rules
-    tags =   [(s_tag, [(t_tag,
-                        flatten t
+    positive_tags =   [(s_tag, [(t_tag,
+                                 flatten t
+                                )
+                               | (tn,t) <- numbered ts,let t_tag = s_tag ++ "t" ++ show tn]
                        )
-                      | (tn,t) <- numbered ts,let t_tag = s_tag ++ "t" ++ show tn]
-              )
-             | (sn,ts) <- numbered forests, let s_tag = "s" ++ show sn] :: [(String,[(String,[String])])]
+                      | (sn,ts) <- numbered positive_forests, let s_tag = "s" ++ show sn] :: [(String,[(String,[String])])]
+    negative_tags =   [(s_tag, [(t_tag,
+                                 flatten t
+                                )
+                               | (tn,t) <- numbered ts,let t_tag = s_tag ++ "t" ++ show tn]
+                       )
+                      | (sn,ts) <- numbered negative_forests, let s_tag = "ns" ++ show sn] :: [(String,[(String,[String])])]
     -- List of all sentence variables
-    sentences = map fst tags 
+    positive_sentences = map fst positive_tags
+    negative_sentences = map fst negative_tags 
     -- List of all tree variables
-    trees = [t | (_,ts) <- tags, (t,_) <- ts]
+    positive_trees = [t | (_,ts) <- positive_tags, (t,_) <- ts]
+    negative_trees = [t | (_,ts) <- negative_tags, (t,_) <- ts]
     -- List of all rule names
-    rules = [r | (_,ts) <- tags, (_,rs) <- ts, r <- rs]
+    positive_rules = [r | (_,ts) <- positive_tags, (_,rs) <- ts, r <- rs]
+    negative_rules = [r | (_,ts) <- negative_tags, (_,rs) <- ts, r <- rs]
   in
     execLPM $ do
       setDirection dir
-      setObjective (f tags)
-      geqTo (linCombination [(1,s) | s <- sentences]) $ length sentences
-      sequence_ [geqTo (linCombination ((-1,s):[(1,t) | (t,_) <- ts])) 0 | (s,ts) <- tags]
-      sequence_ [geqTo (linCombination ((-(length rs),t):[(1,r) | r <- rs])) 0 | (_,ts) <- tags,(t,rs) <- ts]
+      setObjective (f positive_tags)
+      geqTo (linCombination [(1,s) | s <- positive_sentences]) $ length positive_sentences
+      leqTo (linCombination [(1,s) | s <- negative_sentences]) 0
+      sequence_ [geqTo (linCombination ((-1,s):[(1,t) | (t,_) <- ts])) 0 | (s,ts) <- positive_tags]
+      sequence_ [leqTo (linCombination ((-1,s):[(1,t) | (t,_) <- ts])) 0 | (s,ts) <- negative_tags]
+      sequence_ [geqTo (linCombination ((-(length rs),t):[(1,r) | r <- rs])) 0 | (_,ts) <- positive_tags,(t,rs) <- ts]
+      sequence_ [leqTo (linCombination ((-(length rs),t):[(1,r) | r <- rs])) (length rs) | (_,ts) <- negative_tags,(t,rs) <- ts]
       sequence_ $
-          [setVarKind s BinVar | s <- sentences] ++
-          [setVarKind t BinVar | t <- trees] ++
-          [setVarKind r BinVar | r <- rules]
+          [setVarKind s BinVar | s <- positive_sentences++negative_sentences] ++
+          [setVarKind t BinVar | t <- positive_trees ++ negative_trees] ++
+          [setVarKind r BinVar | r <- nub (positive_rules ++ negative_rules)]
 
 -- | Objective function to minimize the number of rules
 numRules :: ObjectiveFunction [String]
@@ -60,7 +73,7 @@ test = do
   let forests = examplesToForests grammar (fromJust $ readLanguage "ExemplumEng") examples
   -- create csp
   putStrLn ">>> Convert forests to CSP"
-  let problem = forestsToProblem forests numTrees
+  let problem = forestsToProblem forests [] numTrees
   putStrLn $ ">>> Got problem:\n" ++ show problem
   writeLP "/tmp/problem.lp" problem
   -- solve problem
@@ -100,6 +113,70 @@ test = do
       "it becomes bad already",
       "they don't break her today already today"
       ]
+    testExamples :: Grammar -> Language -> [Example] -> [(String,Bool)]
+    testExamples g l es = 
+      zip es $ map (not.null) $ examplesToForests g l es
+
+
+testNegative :: IO ()
+testNegative =
+  do
+    -- load grammar
+    putStrLn ">>> Load grammar"
+    p <- readPGF $ path_to_exemplum</>"ExemplumEng.pgf"
+    let grammar = Grammar p [path_to_exemplum</>"ExemplumEng.gf"]
+    putStrLn $ ">>> Loaded " ++ (show $ length $ functions p) ++ " Rules"
+      -- convert examples
+    putStrLn ">>> Convert examples to forests"
+    let positive_forests = examplesToForests grammar (fromJust $ readLanguage "ExemplumEng") positive
+    let negative_forests = examplesToForests grammar (fromJust $ readLanguage "ExemplumEng") negative
+    -- create csp
+    putStrLn ">>> Convert forests to CSP"
+    let problem = forestsToProblem positive_forests negative_forests numTrees
+    putStrLn $ ">>> Got problem:\n" ++ show problem
+    writeLP "/tmp/problem.lp" problem
+    -- solve problem
+    putStrLn ">>> Solve the CSP"
+    solution <- solve problem
+    putStrLn $ ">>> Got " ++ (show $ length $ snd solution) ++ " rules with a score of " ++ (show $ fst solution) ++ ": \n" ++ show (snd solution)
+    -- create new grammar
+    putStrLn ">>> Create New Grammar"
+    grammar' <- generateGrammar grammar solution
+    putStrLn $ ">>> Loaded " ++ (show $ length $ functions $ pgf grammar') ++ " Rules"
+    -- check result
+    let testPositiveResults = testExamples grammar' (fromJust $ readLanguage "ExemplumSubEng") positive
+    let testNegativeResults = testExamples grammar' (fromJust $ readLanguage "ExemplumSubEng") negative
+    if ((and $ map snd testPositiveResults) && (all not $ map snd testNegativeResults)) then
+      putStrLn ">>> Success!!!"
+      else
+      do
+        putStrLn $ ">>> Failed covering:\n" ++ (unlines $ map fst $ filter (not . snd) testPositiveResults)
+        putStrLn $ ">>> Accidental covering:\n" ++ (unlines $ map fst $ filter snd testNegativeResults)
+  where
+    positive = [
+      "few bad fathers become big",
+      "now John and Paris aren't good now",
+      "many cold books come today",
+      "now Paris and he today don't read few cold mothers",
+      "John becomes cold",
+      "on Paris now Paris comes",
+      "now the bad cold fathers are big",
+      "every computer doesn't break many mothers now",
+      "many fathers today on Paris don't hit many mothers",
+      "Paris isn't good today today"
+      ]
+    negative = [
+      "it is blue",
+      "they don't love every mother",
+      "it doesn't come",
+      "today she doesn't read Paris now now",
+      "now it doesn't become blue in John",
+      "Paris doesn't switch on it now today now",
+      "today to it they become good now",
+      "to Paris on it today they don't close her now",
+      "it becomes bad already",
+      "they don't break her today already today"
+        ]
     testExamples :: Grammar -> Language -> [Example] -> [(String,Bool)]
     testExamples g l es = 
       zip es $ map (not.null) $ examplesToForests g l es
